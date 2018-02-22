fe06ba15f2958f769e6db89d8e04ac8617d5d692
Minh Nguyen <minh_pham@me.com>
2018-02-22 12:39:40 +0100

    clean something up and implemented get first page

--- src/main/java/com/exxeta/cleancode/demo/CsvNext.java
+++
@@ -1,4 +0,0 @@
-package com.exxeta.cleancode.demo;
-
-public class CsvNext {
-}
--- src/main/java/com/exxeta/cleancode/demo/CsvViewer.java
+++ src/main/java/com/exxeta/cleancode/demo/CsvViewer.java
@@ -3,6 +3,7 @@ package com.exxeta.cleancode.demo;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -23,7 +24,8 @@ public class CsvViewer {
         memory.setPageNumber(Pagination.getFirstPage());
 //        PageManager.extractPage(memory);
         UIViewer uiViewer = new UIViewer();
-        uiViewer.updateView(memory.getAdresses()); //TODO pass only first-page addresses to the view
+        uiViewer.updateView(firstPage()); //TODO pass only first-page addresses to the view
+//        firstPage().forEach(System.out::println);
     }
 
     private String getCsvPath(String [] args) {
@@ -40,23 +42,32 @@ public class CsvViewer {
         AtomicReference<Integer> rowNumber = new AtomicReference<>(0);
         Arrays.stream(args).forEach(s -> {
             if (s.contains("row=")) {
-                rowNumber.set(Integer.valueOf(s.replace("row=", "")));
+                rowNumber.set(Integer.valueOf(s.replace("row=", ""))+1);
             }
         });
         return rowNumber.get();
     }
 
-    public void firstPage() {
+    public List<Adress> firstPage() {
         //UIViewer uiViewer = new UIViewer();
         //uiViewer.updateView(ADDRESSES_WHICH_ARE_ON_THE_FIRST_PAGE);
 
         memory.setPageNumber(Pagination.getFirstPage());
         // calc how many page
         System.out.println("maximal pagination with thins applicaiton is " + memory.getAdresses().size() / memory.getRows());
-        for (int i = 0; i < memory.getRows(); i++) {
-            System.out.println(memory.getAdresses().get(i));
-        }
+        return getPageElements(0, memory.getRows());
+    }
 
+    public List<Adress> getPageElements(int page, int rows) {
+        List<Adress> pageAdress = new ArrayList<>();
+        for (int i = page; i < setPage(rows, page); i++) {
+            pageAdress.add(memory.getAdresses().get(i));
+        }
+        return pageAdress;
+    }
+
+    private int setPage(int rows, int page) {
+        return page == 0 ? rows : rows * page;
     }
 
     public void previousPage() {
